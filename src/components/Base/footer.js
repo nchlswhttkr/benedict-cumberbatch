b9ed449ee943a572966a40c28e8977c7692a96f8
Nicholas Whittaker <NickelOz@users.noreply.github.com>
2018-02-02 17:22:14 +1100

    feat: project version is now displayed

--- src/components/Base/footer.js
+++ src/components/Base/footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { version } from '../../../package.json'
 
 const Footer = props => {
   return (
     <footer style={{ backgroundColor: '#eee', textAlign: 'center', padding: '12px 0' }}>
       <h1>Source Code @ <a href='https://github.com/nickeloz/benedict-cumberbatch'>Github</a></h1>
       <h2>Written by Nicholas Whittaker</h2>
+      <h3>v{version}</h3>
     </footer>
   )
 }
